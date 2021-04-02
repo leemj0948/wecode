cf2c84bb3466391ff38cde2d7d8bacd6bff2c054
leemj0948 <ljo094822@naver.com>
2021-04-02 14:28:57 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+
+   const arrValuses =new Array(); 
+   function pairNum () {
+   
+    for(let i=0;i<51;i++){
+         if(i%2 == 0){
+           arrValuses.push(i)
+         }
+       }
+     console.log(arrValuses)
+   }
+   pairNum()
